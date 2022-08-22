2b4c90fcce057f85bc4851fc7965781ee32d752f
Anurag Pramanik <p.anurag@outlook.com>
2022-08-22 19:10:07 +0530

    Final commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern-desk': "url('./images/pattern-background-desktop.svg')",
+        'hero-pattern-mobile': "url('./images/pattern-background-mobile.svg')",
+      },
+    },
+    colors: {
+      paleBlue: 'hsl(225, 100%, 94%)',
+      brightBlue: 'hsl(245, 75%, 52%)',
+      veryPaleBlue: 'hsl(225, 100%, 98%)',
+      desaturatedBlue: 'hsl(224, 23%, 55%)',
+      darkBlue: 'hsl(223, 47%, 23%)',
+    },
   },
   plugins: [],
 }
